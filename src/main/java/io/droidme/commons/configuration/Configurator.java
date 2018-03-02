81807903e2a885a74d624f22ecc70a7645b560ae
droidme <reinhard@merfeld.info>
2018-03-02 21:34:58 +0100

    configurator restful

--- src/main/java/io/droidme/commons/configuration/Configurator.java
+++ src/main/java/io/droidme/commons/configuration/Configurator.java
@@ -2,21 +2,36 @@ package io.droidme.commons.configuration;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
 import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.AnnotatedField;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import org.slf4j.Logger;
 
 /**
  *
  * @author droidme
  */
+@Singleton
+@Path("configurator")
+@javax.ws.rs.Produces(MediaType.TEXT_PLAIN)
 public class Configurator {
 
     private Properties configuration;
@@ -107,6 +122,38 @@ public class Configurator {
                     .getMember().getName();
             return clazzName + "." + memberName;
         }
+    }
+    
+    @GET
+    public String getConfiguration() {
+        return this.configuration.toString();
+    }
+    
+    @GET
+    @Path("{key}")
+    public String getEntry(@PathParam("key") String key) {
+        return configuration.getProperty(key);
+    }
+    
+    @PUT
+    @Path("{key}")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response addEntry(@PathParam("key") String key, String value, 
+            @Context UriInfo uriInfo) {
+        if (configuration.containsKey(key)) {
+            return Response.noContent().build();
+        } else {
+            configuration.put(key, value);
+            URI uri = uriInfo.getAbsolutePathBuilder().build(key);
+            return Response.created(uri).build();
+        }
+    }
+    
+    @DELETE
+    @Path("{key}")
+    public Response deleteEntry(@PathParam("key") String key) {
+        configuration.remove(key);
+        return Response.noContent().build();
     }
 
 }
